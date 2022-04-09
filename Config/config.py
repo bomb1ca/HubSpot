0b31cb5752f035f6e4f8e32063eaefaa89853c1a
Milan Radovic <milan.radovic88@yahoo.com>
2022-04-09 11:54:14 +0200

    Initial commit

---
+++ Config/config.py
@@ -0,0 +1,4 @@
+class TestData:
+    CHROME_EXECUTABLE_PATH = "C:\Program Files (x86)\drajveri\drajveri\chrome\chromedriver"
+    FIREFOX_EXECUTABLE_PATH = "C:\Program Files (x86)\drajveri\drajveri\gecko\geckodriver"
+
